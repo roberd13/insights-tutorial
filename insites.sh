b84c9870f235d8d917e2da6a8b136ca0eeca45bc
Donnie Roberson <roberd13@users.noreply.github.com>
2018-12-07 15:16:49 -0500

    Update insites.sh

--- insites.sh
+++ insites.sh
@@ -5,8 +5,8 @@
 mkdir promethius
 
 wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/prometheus/prometheus.yml -O promethius/prometheus.yml
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/prometheus/tg_dse.json -O promethius/tg_dse.json
-sed -ri 's/(127.0.0.1:9103).*/\1 '"dse-server"'tg_dse.json' 
+# Download tg_dse.json preconfigured for insites tutorial
+wget -L https://raw.githubusercontent.com/roberd13/insights-tutorial/master/tg_dse.json -O promethius/tg_dse.json
 
 # Download the grafana configs
 
@@ -22,3 +22,6 @@ wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboard
 wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/prometheus-metrics.json -O grafana/dashboards/prometheus-metrics.json
 wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/system-metrics.json -O grafana/dashboards/system-metrics.json
 
+#Get promethius conf for dse 
+
+wget -L https://raw.githubusercontent.com/roberd13/insights-tutorial/master/prometheus.conf -O prometheus.conf
