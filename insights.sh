d4cdebedbdb8e6cb3504072e3b31a9aa0361c966
Donnie Roberson <roberd13@users.noreply.github.com>
2018-12-10 13:12:57 -0500

    Update insights.sh

--- insights.sh
+++ insights.sh
@@ -4,27 +4,27 @@
 
 mkdir prometheus
 
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/prometheus/prometheus.yml -O prometheus/prometheus.yml
+curl https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/prometheus/prometheus.yml -o prometheus/prometheus.yml
 # Download tg_dse.json preconfigured for insites tutorial
-wget -L https://raw.githubusercontent.com/roberd13/insights-tutorial/master/tg_dse.json -O prometheus/tg_dse.json
+curl https://raw.githubusercontent.com/roberd13/insights-tutorial/master/tg_dse.json -o prometheus/tg_dse.json
 
 # Download the grafana configs
 
 mkdir grafana
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards.yaml -O grafana/dashboards.yaml
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/prometheus-datasource.yaml -O grafana/prometheus-datasource.yaml
+curl https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards.yaml -o grafana/dashboards.yaml
+curl https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/prometheus-datasource.yaml -o grafana/prometheus-datasource.yaml
 
 # Makd dashboards directory
 mkdir grafana/dashboards
 
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/dse-cluster-condensed.json -O grafana/dashboards/dse-cluster-condensed.json
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/dse-cluster-metrics.json -O grafana/dashboards/dse-cluster-metrics.json
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/prometheus-metrics.json -O grafana/dashboards/prometheus-metrics.json
-wget -L https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/system-metrics.json -O grafana/dashboards/system-metrics.json
+curl https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/dse-cluster-condensed.json -o grafana/dashboards/dse-cluster-condensed.json
+curl https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/dse-cluster-metrics.json -o grafana/dashboards/dse-cluster-metrics.json
+curl https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/prometheus-metrics.json -o grafana/dashboards/prometheus-metrics.json
+curl https://raw.githubusercontent.com/datastax/dse-metric-reporter-dashboards/master/grafana/dashboards/system-metrics.json -o grafana/dashboards/system-metrics.json
 
 # Download promethius conf for dse 
 
-wget -L https://raw.githubusercontent.com/roberd13/insights-tutorial/master/prometheus.conf -O prometheus.conf
+curl https://raw.githubusercontent.com/roberd13/insights-tutorial/master/prometheus.conf -o prometheus.conf
 
 # Download Docker compose yaml
-wget -L https://raw.githubusercontent.com/roberd13/insights-tutorial/master/insights-compose.yaml -O insights-compose.yaml
+curl https://raw.githubusercontent.com/roberd13/insights-tutorial/master/insights-compose.yaml -o insights-compose.yaml
